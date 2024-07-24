ff83e755828ecaeededfc28f09a700c7becad997
ANNI-74259 <maheshwarianirudh07@gmail.com>
2024-07-24 11:42:24 +0000

    datatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,17 @@
+const accountId= 12321
+let accountEmail="anni@gmail.com"
+var accountPassword="12345"
+accountCity= "jaipur"
+let accountState;
+
+// accountId= 2  //not allowed
+
+accountEmail="ani@gmail.com"
+accountPassword="121212"
+accountCity="ksg"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/* prefer not sto ues var
+because of issue in block scope and functional scope
+*/
