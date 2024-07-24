d651850a9c4f2c07338849b2a619ff05eb2df51c
ANNI-74259 <maheshwarianirudh07@gmail.com>
2024-07-24 14:20:48 +0000

    datatype,memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++++++++++++++++++++//
+
+//stack(primitive),heap(non-primitive/refrence)
+
+let myYoutubename="techknowledg.com"
+
+let anothername=myYoutubename
+anothername="knowledgehub"
+
+console.log(anothername)
+console.log(myYoutubename)
+
+let userone= {
+    email:"ani@google.com" ,
+    upi:"ani@ybl" 
+}
+
+
+let usertwo=userone
+
+usertwo.email="nai@google.com"
+
+console.log(userone)
+console.log(usertwo)
