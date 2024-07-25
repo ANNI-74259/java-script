88408fb5e721d8c94b3ff3fb125f6a8b79a28a9d
ANNI-74259 <maheshwarianirudh07@gmail.com>
2024-07-25 06:12:25 +0000

    about string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+const name="anni"
+const repocount=50
+
+// console.log(name+repocount+"value");
+
+console.log(`my name is ${name}and my repocount is${repocount}`);//string interpulation, this is the mordern way to combine string
+
+const gameName=new String('anniam')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'));
