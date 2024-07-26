a565d2e4bf6b39676c94d26eae9e445385fb3016
ANNI-74259 <maheshwarianirudh07@gmail.com>
2024-07-26 07:01:16 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+// array
+ const Arr= [0,1,2,3,4,5]
+ const mixArr = [1,2,"anni","sam"]
+
+ console.log(Arr);
+ //console.log(Arr[4]);
+ //console.log(mixArr);
+
+//Array methods
+// Arr.push(7)
+// Arr.push(9)
+// Arr.pop()
+
+// Arr.unshift(9)
+// myArr.shift()
+
+
+// console.log(Arr.includes(5));
+// console.log(Arr.indexOf(3));
+
+const nArr=Arr.join()
+
+// console.log(Arr);
+// console.log(nArr);
+
+//   slice and splice
+
+console.log("a ", Arr );
+const nr1 =Arr.slice(1,3)
+console.log(nr1)
+
+console.log("b ", Arr );
+const nr2= Arr.splice(1,3)
+console.log(nr2 );
+
+console.log("c ", Arr );
+console.log("a ", Arr );
+console.log(nr2 );
+console.log(nr1)
+
+
+
