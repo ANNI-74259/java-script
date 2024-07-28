e6aedb1ec60ebbec631351832c645a8f65fcd0a1
ANNI-74259 <maheshwarianirudh07@gmail.com>
2024-07-28 07:43:42 +0000

    about arrow and this keyword

---
+++ 03_basics/03_arrow&this .js
@@ -0,0 +1,59 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username); //we can't ues this like object
+// }
+
+
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (3, 4) =>  num1 + num2
+
+//const addTwo = (num1, num2) => ( num1 + num2 )
+//these both are implicite return means we dont need to write return
+
+//const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach() 
