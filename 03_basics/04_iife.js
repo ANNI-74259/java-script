6154f6e13d480ce005b78f4e36361766d4403f75
ANNI-74259 <maheshwarianirudh07@gmail.com>
2024-07-28 07:52:28 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+//ek iife ke baad dusra iife likhne ke liye first iife ke baad ; lagate hai otherwise error show karta hai  imp*
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
+
+//globle scope se polution nhi chhiye 0r immediately execute ho jaye so we ues iife  [()()]**
